053ac4edaaa90f8540d054cadd4646ec7bd07768
mehrlander <39290383+mehrlander@users.noreply.github.com>
2025-12-11 13:01:47 -0800

    Add 'HELLO' div to jse component

--- components/jse.js
+++ components/jse.js
@@ -2,6 +2,7 @@
 import { alp } from '../core.js';
 alp.define('jse', _ => `
   <div class="flex flex-col h-full bg-base-100 overflow-hidden relative isolate">
+    <div>HELLO</div>
     <div name="jse" class="relative flex-1 overflow-hidden min-h-0"></div>
     <div class="flex bg-base-300 text-xs flex-shrink-0 p-2 items-center gap-2 border-t border-base-200">
       <template x-if="namespaces.length > 1">
