4d18d6fecf9e3c93bd751b11ce72a0aae0b546c6
mehrlander <39290383+mehrlander@users.noreply.github.com>
2025-12-11 11:56:14 -0800

    Remove commented components from index.js

--- components/index.js
+++ components/index.js
@@ -1,10 +1,6 @@
 export const components = [
   'inspector.js',
   'bill-table.js',
-  // Generalized wrapper components
   'tb.js',
-  'jse.js',
-  // Demo components showing nested usage
-  'tb-demo.js',
-  'jse-demo.js'
+  'jse.js'
 ];
