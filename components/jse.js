5f79681795edd048bb166059482b19b7514b5726
mehrlander <39290383+mehrlander@users.noreply.github.com>
2025-12-30 10:08:42 -0800

    Remove unused import from jse.js

--- components/jse.js
+++ components/jse.js
@@ -1,5 +1,4 @@
 // components/jse.js - Lean JSON Editor wrapper
-import { alp } from '../core.js';
 alp.define('jse', _ => `
   <div class="flex flex-col h-full bg-base-100 overflow-hidden">
     <div class="flex-1 min-h-0">
